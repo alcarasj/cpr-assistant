5dfb6cd0d6222314045567e370f2ba26556b57ba
Jerico Alcaras <alcarasj@tcd.ie>
2019-03-12 14:14:16 +0000

    Refactored code.

--- opencv-python-test/dense_optical_flow.py
+++ opencv-python-test/dense_optical_flow.py
@@ -3,46 +3,58 @@ import numpy as np
 
 
 THRESHOLD = 1
+SCALE = 0.5
 LEARNING_RATE = 0.005
-cap = cv.VideoCapture('Ken1BUV.mp4')
-ret, frame1 = cap.read()
-frame1 = cv.resize(frame1, (0, 0), fx=0.5, fy=0.5) 
-prvs = cv.cvtColor(frame1,cv.COLOR_BGR2GRAY)
-hsv = np.zeros_like(frame1)
-hsv[...,1] = 255
-weights = np.zeros_like(frame1[..., 0])
-compressions = 0
+VIDEO_DIR = 'Ken1BUV.mp4'
 
-while True:
-    ret, frame2 = cap.read()
-    orig = frame2
-    frame2 = cv.resize(frame2, (0, 0), fx=0.5, fy=0.5)
+def main():
+    video = cv.VideoCapture(VIDEO_DIR)
+    (ret, next_frame_bgr) = video.read()
+    next_frame_bgr = cv.resize(next_frame_bgr, (0, 0), fx=SCALE, fy=SCALE) 
+    hsv = np.zeros_like(next_frame_bgr)
+    hsv[...,1] = 255
+    compressions = 0
+    next_frame_gray = cv.cvtColor(next_frame_bgr,cv.COLOR_BGR2GRAY)
+    prev_frame_bgr = np.array([])
 
-    # Dense optical flow.
-    next = cv.cvtColor(frame2,cv.COLOR_BGR2GRAY)
-    flow = cv.calcOpticalFlowFarneback(prev=prvs, next=next, flow=None, pyr_scale=0.5, levels=3, winsize=20, iterations=3, poly_n=5, poly_sigma=1.2, flags=0)
-    mag, ang = cv.cartToPolar(flow[...,0], flow[...,1])
-    #mag = np.array([[p if p >= THRESHOLD else 0 for p in row] for row in mag])
-    #weights = np.array([[((1-LEARNING_RATE) * weight) + (LEARNING_RATE * mag[i][j]) for j, weight in enumerate(row)] for i, row in enumerate(weights)])
-    #import pdb; pdb.set_trace()
-    if np.sum(mag) > 185000:
-        compressions += 1
-    hsv[...,0] = ang*180/np.pi/2
-    hsv[...,2] = cv.normalize(mag,None,0,255,cv.NORM_MINMAX)
-    bgr = cv.cvtColor(hsv,cv.COLOR_HSV2BGR)
-    cv.namedWindow('Video', cv.WINDOW_NORMAL)
-    cv.namedWindow('Flow', cv.WINDOW_NORMAL)
-    cv.putText(orig, "(%i, %i)" % (np.sum(mag), np.mean(ang*180/np.pi/2)), (150,300), cv.FONT_HERSHEY_SIMPLEX, 2, 255, thickness=5)
-    cv.putText(orig, "CMP: %i" % compressions, (150,200), cv.FONT_HERSHEY_SIMPLEX, 2, 255, thickness=5)
-    cv.imshow('Video', orig)
-    cv.imshow('Flow',bgr)
-    k = cv.waitKey(30) & 0xff
-    if k == 27:
-        break
-    elif k == ord('s'):
-        cv.imwrite('opticalfb.png',frame2)
-        cv.imwrite('opticalhsv.png',bgr)
-    prvs = next
+    while True:
+        if prev_frame_bgr.any():
+            (ret, next_frame_bgr) = video.read()
 
-cap.release()
-cv.destroyAllWindows()
+            # Pre-processing to eliminate noise.
+            next_frame_bgr = cv.resize(next_frame_bgr, (0, 0), fx=SCALE, fy=SCALE)
+            next_frame_bgr = cv.GaussianBlur(next_frame_bgr, (5, 5), 5)
+
+            # Dense optical flow.
+            prev_frame_gray = cv.cvtColor(prev_frame_bgr, cv.COLOR_BGR2GRAY)
+            next_frame_gray = cv.cvtColor(next_frame_bgr, cv.COLOR_BGR2GRAY)
+            flow = cv.calcOpticalFlowFarneback(prev=prev_frame_gray, next=next_frame_gray, flow=None, pyr_scale=0.5, levels=3, winsize=25, iterations=1, poly_n=5, poly_sigma=1.2, flags=0)
+            magnitude, direction = cv.cartToPolar(flow[...,0], flow[...,1])
+            np.place(magnitude, magnitude <= THRESHOLD, 0)
+            direction_in_deg = direction * 180 / np.pi / 2
+            mean_direction = np.mean(direction_in_deg)
+
+            # Compression detection.
+            if np.sum(magnitude) > 185000 and mean_direction < 90:
+                compressions += 1
+
+            # Output windows.
+            original = next_frame_bgr
+            hsv[...,0] = direction_in_deg
+            hsv[...,2] = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX) 
+            flow_bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+            cv.namedWindow('Video', cv.WINDOW_NORMAL)
+            cv.namedWindow('Flow', cv.WINDOW_NORMAL)
+            cv.imshow('Video', original)
+            cv.imshow('Flow', flow_bgr)
+            k = cv.waitKey(30) & 0xff
+            if k == 27:
+                break
+
+        prev_frame_bgr = next_frame_bgr
+
+    video.release()
+    cv.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
