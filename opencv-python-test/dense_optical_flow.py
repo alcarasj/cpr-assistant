72f50392b22d36bd9b95550e189ea01177d940c9
Jerico Alcaras <alcarasj@tcd.ie>
2019-03-22 18:19:59 +0000

    5-frame average for smoothing.

--- opencv-python-test/dense_optical_flow.py
+++ opencv-python-test/dense_optical_flow.py
@@ -1,27 +1,82 @@
 import cv2
 import numpy as np
+import imutils
+import os
+import csv
 import matplotlib 
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
 WEBCAM_MODE = False
-VIDEO_DIR = './Ken1BUV.mp4'
+READ_ONLY = True
+INPUT_VIDEO = './Ken1BUV.mp4'
 MIN_TIME_BETWEEN_MAXIMUMS = 0.2
 STRIDE = 8
 MIN_FLOW_THRESHOLD = 0.75
-WEIGHTING_THRESHOLD = 0.25
+WEIGHTING_THRESHOLD = 0.1
 START_FRAME = 146
 SCALE = 0.33
 LEARNING_RATE = 0.008
-VIDEO = cv2.VideoCapture(0 if WEBCAM_MODE else VIDEO_DIR)
+VIDEO = cv2.VideoCapture(0 if WEBCAM_MODE else INPUT_VIDEO)
 FPS = VIDEO.get(cv2.CAP_PROP_FPS)
 NUMBER_OF_FRAMES = int(VIDEO.get(cv2.CAP_PROP_FRAME_COUNT))
 DURATION = float(NUMBER_OF_FRAMES - START_FRAME) / float(FPS)
 TEXT_START_POS_Y = 30
+CSV_DIR = 'csv_results/%s.csv'
 
-def main():
 
-    data = []
+def plot_data(data):
+    """ Plots the raw data (x, y) data into a graph. """
+
+    print("Plotting data...")
+    frames = list(range(1, len(data) + 1))
+    movement = [coord[1] for coord in data]
+    plt.plot(frames, movement)
+    plt.ylabel('Movement')
+    plt.xlabel('Frame')
+    plt.axis([0, len(data) * 1.5, -5000, 5000])
+    plt.show()
+
+
+def read_from_csv(csv_file):
+    """ Reads data from a CSV file. """
+
+    print("Reading from CSV file...")
+    coords = []
+
+    reader = csv.reader(csv_file, delimiter=',')
+    for row in reader:
+        coords.append((float(row[0])/ 100, int(row[1])/ 100))
+
+    print("Reading from CSV file complete! %i co-ordinates read." % len(coords))
+    return coords
+
+
+def write_to_csv(data):
+    """ Writes the data into a CSV file. """
+    
+    print("Writing to CSV file...")
+
+    if os.path.exists(CSV_DIR % INPUT_VIDEO):
+        os.remove(CSV_DIR % INPUT_VIDEO)
+
+    try:
+        os.stat('csv_results')
+    except:
+        os.mkdir('csv_results')
+    
+    csv_file = open(CSV_DIR % INPUT_VIDEO, mode='w')
+    writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    row_count = 0
+
+    for coord in data:
+        writer.writerow([coord[0], coord[1], coord[2]])
+
+    csv_file.close()
+    print("Write to CSV file complete! %i/%i datapoints written." % (row_count, len(data))) 
+
+
+def process_video():
 
     # Initial values.
     (ret, current_frame_bgr) = VIDEO.read()
@@ -47,6 +102,8 @@ def main():
     print("PROCESSED RESOLUTION: %ix%i" % (len(current_frame_bgr[0]), len(current_frame_bgr)))
     total_pixels = len(current_frame_bgr[0]) * len(current_frame_bgr)
     print("TOTAL PIXELS: %i" % total_pixels)
+
+    data = []
 
     # Processing loop.
     while True:
@@ -87,12 +144,12 @@ def main():
             upward_sum = np.sum(upward_movement)
 
             total_movement_pcg = (np.sum(np.where(downward_movement > 0)[0]) + np.sum(np.where(upward_movement > 0)[0])) / total_pixels
-            resolved_up_down = upward_sum - downward_sum
+            vertical_resultant = upward_sum - downward_sum
+            five_frame_avg = sum([val[0] for val in data[-5:]]) / 5
 
-            data.append((resolved_up_down, total_movement_pcg))
+            data.append((vertical_resultant, int(five_frame_avg), int(total_movement_pcg)))
 
             if current_frame_number > START_FRAME or WEBCAM_MODE:
-
                 # Calculate weights for isolation of zone with high movement density.
 
                 old_weights = weights * (1 - LEARNING_RATE)
@@ -106,7 +163,7 @@ def main():
                 elapsed_time = float(current_frame_number - START_FRAME) / float(FPS)
 
                 # Compression detection.
-                if resolved_up_down > prev_resultant and minimum == prev_resultant:
+                if vertical_resultant > prev_resultant and minimum == prev_resultant:
                     compressions += 1
                     minimum = 0
 
@@ -120,7 +177,7 @@ def main():
                             mean_ccr = np.mean(ccr_data)
 
                     prev_compression_time = elapsed_time
-                if resolved_up_down < prev_resultant:
+                if vertical_resultant < prev_resultant:
                     minimum = prev_resultant
 
             # Outputs.
@@ -137,12 +194,12 @@ def main():
             weights_hsv[..., 2] = cv2.normalize(weights_mask, None, 0, 255, cv2.NORM_MINMAX)
             weights_bgr = cv2.cvtColor(weights_hsv, cv2.COLOR_HSV2BGR)
 
-            print("[%i] = %i (%ipc) MIN=%i" % (current_frame_number, resolved_up_down, total_movement_pcg, minimum))
+            print("[%i] = %i (%ipc) 5FA=%i" % (current_frame_number, vertical_resultant, total_movement_pcg, five_frame_avg))
 
             cv2.putText(flow_bgr, "Time: %f" % elapsed_time, (25, TEXT_START_POS_Y), cv2.FONT_HERSHEY_SIMPLEX, 1.5 * SCALE, (255,255,255), thickness=1)
             cv2.putText(flow_bgr, "CCR: %fbpm" % ccr, (25, TEXT_START_POS_Y + 30), cv2.FONT_HERSHEY_SIMPLEX, 1.5 * SCALE, (255,255,255), thickness=1)
             cv2.putText(flow_bgr, "AVGCCR: %fbpm" % mean_ccr, (25, TEXT_START_POS_Y + 60), cv2.FONT_HERSHEY_SIMPLEX, 1.5 * SCALE, (255,255,255), thickness=1)
-            cv2.putText(flow_bgr, "VECTOR: %f" % resolved_up_down, (25, TEXT_START_POS_Y + 90), cv2.FONT_HERSHEY_SIMPLEX, 1.5 * SCALE, (255,255,255), thickness=1)
+            cv2.putText(flow_bgr, "VECTOR: %f" % vertical_resultant, (25, TEXT_START_POS_Y + 90), cv2.FONT_HERSHEY_SIMPLEX, 1.5 * SCALE, (255,255,255), thickness=1)
             cv2.putText(flow_bgr, "Nc: %i" % compressions, (25, TEXT_START_POS_Y + 150), cv2.FONT_HERSHEY_SIMPLEX, 1.5 * SCALE, (255,255,255), thickness=1)
             cv2.namedWindow('Video', cv2.WINDOW_NORMAL)
             cv2.namedWindow('Flow (All)', cv2.WINDOW_NORMAL)
@@ -158,20 +215,28 @@ def main():
             if k == 27:
                 break
 
-            prev_resultant = resolved_up_down
+            prev_resultant = vertical_resultant
 
         prev_frame_bgr = current_frame_bgr
 
     VIDEO.release()
     cv2.destroyAllWindows()
+    return data
 
-    frames = list(range(1, len(data) + 1))
-    movement = [coord[0] for coord in data]
-    plt.plot(frames, movement)
-    plt.ylabel('Movement')
-    plt.xlabel('Frame')
-    plt.axis([0, len(data), min(*movement), max(*movement)])
-    plt.show()
+
+def main():
+    if not READ_ONLY:
+        data = process_video()
+        write_to_csv(data)
+    else:
+        try:
+            existing_csv = open(CSV_DIR % INPUT_VIDEO)
+            data = read_from_csv(existing_csv)
+        except FileNotFoundError:
+            data = process_video()
+            write_to_csv(data)
+
+    plot_data(data)
 
 if __name__ == '__main__':
     main()
