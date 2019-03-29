22868b9d9d1ff2eb3cf2699691d32f86f4123bb2
Jerico Alcaras <alcarasj@tcd.ie>
2019-03-29 22:50:40 +0000

    Updated detection model to acceleration.

--- opencv-python-test/dense_optical_flow.py
+++ opencv-python-test/dense_optical_flow.py
@@ -13,15 +13,46 @@ parser = ArgumentParser(description='Compressions/breaths detector for CPR using
 parser.add_argument('-d', '--dataset', dest='dataset', help='The name of the dataset e.g. Jerico1.', type=str, required=True)
 parser.add_argument('-r', '--recalculate', dest='recalculate', help='Boolean flag to recalculate if there is any existing CSV (also overwrites existing CSV).', action='store_true')
 parser.add_argument('-o', '--output', dest='output', help='Boolean flag to show output windows.',  action='store_true')
+parser.add_argument('-w', '--weights-file', dest='weights_file', help='Relative path to an .npy weights file to preload weights before processing.', type=str, required=False)
+parser.add_argument('-s', '--save-weights', dest='save_weights', help='Save the weights after processing.', action='store_true')
 args = parser.parse_args()
 
-# Script execution configuration constants.
+
+""""
+HYPERPARAMETERS (all times are in seconds).
+
+BREATHING_MODE - Detects breathing if True.
+LEARNING_RATE - Learning rate for a weighted mask for isolation of user in frame (experimental ideal is 0.0065).
+LOOKBACK_TIME - The time for which the algorithm calculates derivatives of movement (velocity and acceleration).
+MINIMUM_ACCELERATION - The minimum acceleration (in pixels/s/s) required for detecting a compression.
+MOVING_AVG_PERIOD - The moving average period length.
+MAX_TIME_FOR_UPWARD_ACCELERATION - The max allowed time for an upward acceleration to be detected as a compression from when a strong downward acceleration was detected.
+MIN_MOVEMENT_PCG - The minimum percentage of pixels moved to be considered valid for compression detection.
+MIN_FLOW_THRESHOLD - The minimum flow magnitude to be considered a non-noisy movement.  
+SCALE - The scale factor for the frame before processing.
+
+"""
+BREATHING_MODE = True  
+LEARNING_RATE = 0.0065
+LOOKBACK_TIME = 0.2
+MINIMUM_ACCELERATION = 3000
+MOVING_AVG_PERIOD = 0.2
+MAX_TIME_FOR_UPWARD_ACCELERATION = 0.5
+MIN_MOVEMENT_PCG = 15
+MIN_FLOW_THRESHOLD = 0.2
+SCALE = 0.2
+MIN_BREATHING_MOVEMENT = 5000
+
+# Arguments. 
 DATASET = args.dataset
 RECALCULATE = args.recalculate
 SHOW_OUTPUT = args.output
+WEIGHTS_FILE = args.weights_file
+SAVE_WEIGHTS = args.save_weights
 
 # Files and miscellaneous constants.
 INPUT_VIDEO = DATASET + 'BUV.mp4'
+WEIGHTS_DIR = "./weights/%s.npy" % DATASET 
 VIDEO = cv2.VideoCapture(INPUT_VIDEO)
 FPS = int(VIDEO.get(cv2.CAP_PROP_FPS))
 NUMBER_OF_FRAMES = int(VIDEO.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -31,24 +62,33 @@ CSV_DIR = 'csv_results/%s.csv' % INPUT_VIDEO
 GT_DIR = DATASET + 'GT.mp4'
 GT_VIDEO = cv2.VideoCapture(GT_DIR)
 GT_CSV_DIR = './csv_gt/' + DATASET + 'GT.mp4.csv'
+AVERAGING_FRAMES = int(MOVING_AVG_PERIOD * FPS)
+LOOKBACK_FRAMES = int(LOOKBACK_TIME * FPS)
 
-# Global constants for calculations (all times are in seconds).
-LEARNING_RATE = 0.0075
-MAX_ALLOWED_TIME_FOR_UPWARD_MOVEMENT = 0.7
-BREATHS_MODE = True
-MIN_FLOW_THRESHOLD = 0.15
-SCALE = 0.2
-MIN_RESULTANT = 500
-MIN_BREATHING_MOVEMENT = 5000
-MIN_MOVEMENT_PCG = 20
-AVERAGING_TIME = 0.15
-AVERAGING_FRAMES = int(AVERAGING_TIME * FPS)
 
+def save_weights(weights):
+    """ Saves the weights after the processing of video. """
+
+    if os.path.exists(WEIGHTS_DIR):
+        os.remove(WEIGHTS_DIR)
+
+    try:
+        os.stat('weights')
+    except:
+        os.mkdir('weights')
+
+    np.save(WEIGHTS_DIR, weights)
+    print("Weights saved successfully!") 
+
+
+def load_weights(weights_file):
+    """ Preloads existing weights for use in the processing of video. """
+
+    return np.load(weights_file)
 
 
 def plot_data(data, ground_truth=None):
     """ Plots data on a graph. """
-
     print("Plotting data...")
     window, chart = plt.subplots(2, sharex=True)
     window.suptitle("DATASET: " + DATASET)
@@ -56,16 +96,16 @@ def plot_data(data, ground_truth=None):
     # Chart for solution.
     frames = list(range(0, len(data)))
     time_in_seconds = [(i / FPS) for i in frames]
-    movement = [coord[0] if not coord[3] or coord[3] == "Compression" else None for coord in data]
-    breathing = [coord[0] if coord[3] == "Breathing" or coord[3] == "Compression" else None for coord in data]
-    compressions = [coord[0] if coord[3] == "Compression" else None for coord in data]
+    movement_delta = [coord[4] if not coord[3] or coord[3] == "Compression" else None for coord in data]
+    breathing = [coord[4] if coord[3] == "Breathing" or coord[3] == "Compression" else None for coord in data]
+    compressions = [coord[4] if coord[3] == "Compression" else None for coord in data]
     chart[0].set_title('Solution')
-    chart[0].plot(time_in_seconds, movement, "y-", color="orange")
+    chart[0].plot(time_in_seconds, movement_delta, "y-", color="orange")
     chart[0].plot(time_in_seconds, breathing, "y:", color="orange")
     chart[0].plot(time_in_seconds, compressions, "r*")
     chart[0].legend(["Optical Flow", "Optical Flow (Breathing)", "Detected Compression (N=%i)" % len([c for c in compressions if c])])
-    chart[0].set_ylabel('Vertical Displacement (Pixels)')
-    chart[0].axis([0, time_in_seconds[-1], -8000, 8000 ])
+    chart[0].set_ylabel('Vertical Acceleration (pixels/s/s)')
+    chart[0].axis([0, time_in_seconds[-1], np.min([c for c in movement_delta if c]) * 1.2, np.max([c for c in movement_delta if c]) * 1.2])
 
     # Chart for ground truth.
     frames = list(range(0, len(ground_truth)))
@@ -80,7 +120,7 @@ def plot_data(data, ground_truth=None):
     chart[1].legend(["Ground Truth", "Ground Truth (Breathing)", "True Compression (N=%i)" % len([c for c in compressions if c])])
     chart[1].set_ylabel('Y Co-ordinates')
     chart[1].set_xlabel('Time (Seconds)')
-    chart[1].axis([0, time_in_seconds[-1], 200, 800])
+    chart[1].axis([0, time_in_seconds[-1], np.min([c for c in movement if c]) * 1.2, np.max([c for c in movement if c]) * 1.2])
     
     plt.show()
 
@@ -93,7 +133,10 @@ def read_from_csv(csv_file):
 
     reader = csv.reader(csv_file, delimiter=',')
     for row in reader:
-        data.append((float(row[0]) if row[0] != '-' else None, float(row[1]) if row[1] != '-' else None, row[2], row[3] if row[3] != 'None' else None))
+        try:
+            data.append((float(row[0]) if row[0] != '-' else None, float(row[1]) if row[1] != '-' else None, row[2], row[3] if row[3] != 'None' else None, float(row[4])))
+        except IndexError:
+            data.append((float(row[0]) if row[0] != '-' else None, float(row[1]) if row[1] != '-' else None, row[2], row[3] if row[3] != 'None' else None))
 
     print("Reading from CSV file complete! %i co-ordinates read." % len(data))
     return data
@@ -117,14 +160,14 @@ def write_to_csv(data):
     row_count = 0
 
     for coord in data:
-        writer.writerow([coord[0], coord[1], coord[2], coord[3] if coord[3] else "None"])
+        writer.writerow([coord[0], coord[1], coord[2], coord[3] if coord[3] else "None", coord[8]])
         row_count += 1
 
     csv_file.close()
     print("Write to CSV file complete! %i/%i datapoints written." % (row_count, len(data))) 
 
 
-def process_video(ground_truth):
+def process_video(ground_truth, preloaded_weights=None):
     """ The core logic of this dissertation's solution - process the video to detect compressions. """
 
     # Initial values for use in calculations and output.
@@ -138,20 +181,24 @@ def process_video(ground_truth):
     hsv[...,1] = 255
     current_frame_gray = cv2.cvtColor(current_frame_bgr,cv2.COLOR_BGR2GRAY)
     prev_frame_bgr = np.array([])
-    weights = np.zeros_like(current_frame_bgr[..., 0]) + 0.5
-    weights_hsv = np.zeros_like(current_frame_bgr)
-    weights_hsv[..., 1] = 0
-    weights_hsv[..., 0] = 0
     prev_compression_time = None
     is_breathing = False
     state = None
     elapsed_time = 0
-    strong_downward_movement_detected = False
+    strong_downward_acceleration_detected = False
     upward_movement_detected_within_allowed_time = False
-    strong_downward_movement_time = 0
+    strong_downward_acceleration_time = 0
     time_diff = 0
     data = []
 
+    # Preload weights or start weights from scratch.
+    weights_hsv = np.zeros_like(current_frame_bgr)
+    weights_hsv[..., 1] = 0
+    weights_hsv[..., 0] = 0
+    if preloaded_weights.any():
+        weights = preloaded_weights * 0.1
+    else: 
+        weights = np.zeros_like(current_frame_bgr[..., 0]) + 0.5
 
     # Initial compression/CCR values.
     compressions = 0
@@ -241,36 +288,43 @@ def process_video(ground_truth):
             leftward_avg = np.mean([frame[5] for frame in last_n_frames]) if data else 0
             rightward_avg = np.mean([frame[6] for frame in last_n_frames]) if data else 0
 
-            # Vertical resultant using averages for detecting compressions.
-            vertical_resultant = upward_avg - downward_avg
+            # Vertical resultant for detecting compressions.
+            vertical_displacement = upward_avg - downward_avg
+
+            # Get vertical delta for detecting compressions.
+            if last_n_frames and len(last_n_frames) >= LOOKBACK_FRAMES:
+                vertical_velocity = vertical_displacement - data[-(LOOKBACK_FRAMES)][0]
+                vertical_acceleration = vertical_velocity - data[-(LOOKBACK_FRAMES)][7]
+            else: 
+                vertical_velocity = 0
+                vertical_acceleration = 0
 
             # Elapsed time from starting frame for calculating CCR.
             elapsed_time = float(current_frame_number / FPS)
 
             # If there is large lateral movement observed, it is breathing.
             lateral_avg = leftward_avg + rightward_avg
-            if (lateral_avg > MIN_BREATHING_MOVEMENT or is_breathing) and compressions > 10:
+            if BREATHING_MODE and (lateral_avg > MIN_BREATHING_MOVEMENT or is_breathing) and compressions > 10:
                 is_breathing = True
                 state = "Breathing"
 
             # COMPRESSION DETECTION.
-            # 1. Detect a strong downward movement in the previous 200ms.
-            # 2. If a strong downward movement is detected, try to detect a strong upward movement in the following 200ms.
-            #    The strong upward movement must be detected within 500ms from the point in time when the strong downward movement was last detected.
+            # 1. Detect a strong downward acceleration in the previous X seconds defined by MOVING_AVG_PERIOD.
+            # 2. If a strong downward acceleration is detected, try to detect a strong upward acceleration in the following Y seconds defined by MAX_TIME_FOR_UPWARD_ACCELERATION.
             # 3. If these conditions are met, then a compression is detected by this algorithm.
             #    Otherwise, the downward movement is set back to False.
 
-            if not strong_downward_movement_detected and vertical_resultant < -(MIN_RESULTANT) and total_movement_pcg > MIN_MOVEMENT_PCG:
-                strong_downward_movement_detected = True
-                strong_downward_movement_time = elapsed_time
-            elif strong_downward_movement_detected and vertical_resultant > (MIN_RESULTANT * 0.8) and total_movement_pcg > MIN_MOVEMENT_PCG and (elapsed_time - strong_downward_movement_time) <= MAX_ALLOWED_TIME_FOR_UPWARD_MOVEMENT:
+            if not strong_downward_acceleration_detected and vertical_acceleration < -(MINIMUM_ACCELERATION) and total_movement_pcg > MIN_MOVEMENT_PCG:
+                strong_downward_acceleration_detected = True
+                strong_downward_acceleration_time = elapsed_time
+            elif strong_downward_acceleration_detected and vertical_acceleration > (MINIMUM_ACCELERATION) and total_movement_pcg > MIN_MOVEMENT_PCG and (elapsed_time - strong_downward_acceleration_time) <= MAX_TIME_FOR_UPWARD_ACCELERATION:
                 upward_movement_detected_within_allowed_time = True
-            elif strong_downward_movement_detected and (elapsed_time - strong_downward_movement_time) > MAX_ALLOWED_TIME_FOR_UPWARD_MOVEMENT:
-                strong_downward_movement_detected = False
+            elif strong_downward_acceleration_detected and (elapsed_time - strong_downward_acceleration_time) > MAX_TIME_FOR_UPWARD_ACCELERATION:
+                strong_downward_acceleration_detected = False
 
-            if strong_downward_movement_detected and upward_movement_detected_within_allowed_time:
+            if strong_downward_acceleration_detected and upward_movement_detected_within_allowed_time:
                 upward_movement_detected_within_allowed_time = False
-                strong_downward_movement_detected = False
+                strong_downward_acceleration_detected = False
                 compressions += 1
                 state = "Compression"
                 is_breathing = False
@@ -284,18 +338,16 @@ def process_video(ground_truth):
 
                 prev_compression_time = elapsed_time
 
-            print("[%i] (%ipc) VRT_R: %i, LAT_R: %i, S: %s" % (current_frame_number, total_movement_pcg, vertical_resultant, lateral_avg, state))
-            data.append([vertical_resultant, int(upward_sum), int(downward_sum), state, int(total_movement_pcg), int(leftward_sum), int(rightward_sum)])
+            print("[%i] (%ipc) VRT_DELTA: %i, LAT_R: %i, S: %s" % (current_frame_number, total_movement_pcg, vertical_acceleration, lateral_avg, state))
+            data.append([vertical_displacement, int(upward_sum), int(downward_sum), state, int(total_movement_pcg), int(leftward_sum), int(rightward_sum), vertical_velocity, vertical_acceleration])
 
             # Update weighted masking model.
             old_weights = weights * (1 - LEARNING_RATE)
             if not is_breathing:
                 weights = old_weights + ((downward_movement + upward_movement) * LEARNING_RATE)
                 weights = cv2.normalize(weights, weights, 0, 1, cv2.NORM_MINMAX)
-            else:
-                weights = old_weights + (weights * LEARNING_RATE)
-
-
+            elif state == "Compression":
+                weights = weights / (1 - LEARNING_RATE)
 
             # Show output windows for visualization.
             if SHOW_OUTPUT:
@@ -320,7 +372,6 @@ def process_video(ground_truth):
                 cv2.putText(flow_bgr, "CCR: %fbpm" % ccr, (25, TEXT_START_POS_Y + 10), cv2.FONT_HERSHEY_SIMPLEX, 1.5 * SCALE, (255,255,255), thickness=1)
                 cv2.putText(flow_bgr, "S: %s" % (state), (25, TEXT_START_POS_Y + 20), cv2.FONT_HERSHEY_SIMPLEX, 1.5 * SCALE, (255,255,255), thickness=1)
                 cv2.putText(flow_bgr, "Nc: %i" % compressions, (25, TEXT_START_POS_Y + 30), cv2.FONT_HERSHEY_SIMPLEX, 1.5 * SCALE, (255,255,255), thickness=1)
-                cv2.putText(flow_bgr, "TDIFF: %f" % time_diff, (25, TEXT_START_POS_Y + 40), cv2.FONT_HERSHEY_SIMPLEX, 1.5 * SCALE, (255,255,255), thickness=1)
                 cv2.namedWindow('Solution', cv2.WINDOW_NORMAL)
                 cv2.namedWindow('Flow (All)', cv2.WINDOW_NORMAL)
                 cv2.namedWindow('Flow (Upward)', cv2.WINDOW_NORMAL)
@@ -346,24 +397,34 @@ def process_video(ground_truth):
 
     VIDEO.release()
     cv2.destroyAllWindows()
-    return data
+    return weights, data
 
 
 def main():
     gt_csv = open(GT_CSV_DIR)
     ground_truth = read_from_csv(gt_csv)
 
+    if WEIGHTS_FILE:
+        preloaded_weights = load_weights(WEIGHTS_FILE)
+    else:
+        preloaded_weights = None
+
     if RECALCULATE:
-        data = process_video(ground_truth)
+        weights, data = process_video(ground_truth, preloaded_weights)
         write_to_csv(data)
+        if SAVE_WEIGHTS:
+            save_weights(weights)
     else:
         try:
             existing_csv = open(CSV_DIR)
             data = read_from_csv(existing_csv)
         except FileNotFoundError:
             print("Existing solution CSV file not found for %s. Recalculating..." % DATASET) 
-            data = process_video(ground_truth)
+            weights, data = process_video(ground_truth, preloaded_weights)
             write_to_csv(data)
+            if SAVE_WEIGHTS:
+                save_weights(weights)
+
     plot_data(data, ground_truth)
 
 if __name__ == '__main__':
