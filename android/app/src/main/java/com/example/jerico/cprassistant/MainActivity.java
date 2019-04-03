06a40829c0ef33918b273b750316bd7ff0afaa31
Jerico Alcaras <alcarasj@tcd.ie>
2019-04-03 23:37:24 +0100

    Initial layout.

--- android/app/src/main/java/com/example/jerico/cprassistant/MainActivity.java
+++ android/app/src/main/java/com/example/jerico/cprassistant/MainActivity.java
@@ -1,37 +1,34 @@
 package com.example.jerico.cprassistant;
 
-import java.util.List;
-
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
-import org.opencv.imgproc.Imgproc;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.view.View.OnTouchListener;
 import android.view.SurfaceView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends Activity implements CvCameraViewListener2 {
     private static final String  TAG              = "MainActivity";
     private Mat                  mRgba;
 
+    private boolean isWorking;
+
     private CameraBridgeViewBase mOpenCvCameraView;
+    private Button mStartButton;
+    private Button mMoreButton;
+    private TextView mCCRTextView;
 
     private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -61,14 +58,34 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
         setContentView(R.layout.activity_main);
+
+        isWorking = false;
 
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.camera_view);
         mOpenCvCameraView.enableFpsMeter();
         mOpenCvCameraView.setCameraIndex(1);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
+
+
+        mCCRTextView = (TextView) findViewById(R.id.ccr_textview);
+        mMoreButton = (Button) findViewById(R.id.more_button);
+        mStartButton = (Button) findViewById(R.id.start_button);
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                isWorking = !isWorking;
+                if (isWorking) {
+                    mMoreButton.setVisibility(View.GONE);
+                    mStartButton.setText("Stop");
+                    mCCRTextView.setText("DETECTING");
+                } else {
+                    mMoreButton.setVisibility(View.VISIBLE);
+                    mStartButton.setText("Start");
+                    mCCRTextView.setText("READY");
+                }
+            }
+        });
     }
 
     @Override
